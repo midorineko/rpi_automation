5dd85243aa8a9c353e2aadc293db6c0d6da3c7d7
midorineko <steven.inouye91@gmail.com>
2016-11-15 20:12:19 -0800

    server check

--- server_check.py
+++ server_check.py
@@ -4,7 +4,7 @@ import time
 hub_name = "inouyehub"
 
 site = "http://%s.localtunnel.me" % hub_name
-open_hub = """x-terminal-emulator -e 'bash -c "lt --port 3000 --subdomain %s"'""" % hub_name
+open_hub = ""'bash -c "lt --port 3000 --subdomain %s"'"" % hub_name
 
 def check():
     try:
@@ -14,13 +14,12 @@ def check():
             import subprocess
             import shlex
             process = subprocess.Popen(
-                shlex.split("""x-terminal-emulator -e 'bash -c "node /home/pi/rpi_automation/app.js"'"""), stdout=subprocess.PIPE)
-           
+                shlex.split(""'bash -c "node /home/pi/rpi_automation/app.js"'), stdout=subprocess.PIPE)
 
             print (process.returncode)
             process2 = subprocess.Popen(
                 shlex.split(open_hub), stdout=subprocess.PIPE)
-            
+
             print (process2.returncode)
     except requests.ConnectionError:
         print("failed to connect")
