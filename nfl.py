9c12db2fefe44769dc9be9171c08d748a9bc88d0
Inouye <inouyes@c4b301cf6c57.ant.amazon.com>
2016-12-06 15:22:11 -0800

    nfl pi now has light stuffs

--- nfl.py
+++ nfl.py
@@ -1,6 +1,10 @@
 import nflgame
 import time
 import sys
+import sys
+from qhue import Bridge
+b = Bridge("192.168.0.100", "elbLovRPUcHaqss904iEJMH9LZrRwsvFeOKSfvOP")
+lights = b.lights
 
 suck_team = ""
 stupidest_var=""
@@ -15,37 +19,58 @@ event_plays=[]
 
 class C:
     def score_method(self, sea_score, sea_score_last, suck_score, suck_score_last):
-        print [sea_score, sea_score_last, suck_score, suck_score_last]
-        cheer_time = True
+        b.lights(1, 'state', bri=255, on=True, xy=[0.6065, 0.3095])
+        b.lights(2, 'state', bri=255, on=True, xy=[0.6883, 0.306])
+        b.lights(3, 'state', bri=255, on=True, xy=[0.6883, 0.306])
+        b.lights(4, 'state', bri=255, on=True, xy=[0.6065, 0.3095])
+        time.sleep(0.25)
+        b.lights(1, 'state', bri=255, on=True, xy=[0.3344, 0.5002])
+        b.lights(2, 'state', bri=255, on=True, xy=[0.3254, 0.5028])
+        b.lights(3, 'state', bri=255, on=True, xy=[0.3239, 0.4932])
+        b.lights(4, 'state', bri=255, on=True, xy=[0.3344, 0.5002])
+        time.sleep(0.25)
+        b.lights(1, 'state', bri=255, on=True, xy=[0.3344, 0.5002])
+        b.lights(2, 'state', bri=255, on=True, xy=[0.3254, 0.5028])
+        b.lights(3, 'state', bri=255, on=True, xy=[0.3239, 0.4932])
+        b.lights(4, 'state', bri=255, on=True, xy=[0.3344, 0.5002])
+        time.sleep(0.25)
+        b.lights(1, 'state', bri=255, on=True, xy=[0.1638, 0.3531])
+        b.lights(2, 'state', bri=255, on=True, xy=[0.2263, 0.3402])
+        b.lights(3, 'state', bri=255, on=True, xy=[0.1618, 0.3365])
+        b.lights(4, 'state', bri=255, on=True, xy=[0.1638, 0.3531])
+        time.sleep(0.25)
+        b.lights(1, 'state', bri=255, on=True, xy=[0.1638, 0.3531])
+        b.lights(2, 'state', bri=255, on=True, xy=[0.2263, 0.3402])
+        b.lights(3, 'state', bri=255, on=True, xy=[0.1618, 0.3365])
+        b.lights(4, 'state', bri=255, on=True, xy=[0.1638, 0.3531])
         if int(sea_score) != int(sea_score_last):
             score_diff = int(sea_score) - int(sea_score_last)
+            print("Good :)")
+            b.lights(2, 'state', bri=255, on=True, xy=[0.1722, 0.0567])
+            b.lights(3, 'state', bri=255, on=True, xy=[0.1722, 0.0567])
         else:
-            cheer_time = False
             score_diff = int(suck_score) - int(suck_score_last)
+            print("Bad :(")
+            b.lights(2, 'state', bri=255, on=True, xy=[0.6883, 0.306])
+            b.lights(3, 'state', bri=255, on=True, xy=[0.6883, 0.306])
         if score_diff == 2:
-            print("safety or two point conversion")
-            if cheer_time:
-                print("good stuff")
-            else:
-                print("bad stuff")
+            print("Safety or two point conversion!")
+            b.lights(1, 'state', bri=255, on=True, xy=[0.6065, 0.3095])
+            b.lights(4, 'state', bri=255, on=True, xy=[0.6065, 0.3095])
         elif score_diff == 3:
-            print(" field goal")
-            if cheer_time:
-                print("good stuff")
-            else:
-                print(" bad stuff")
+            b.lights(1, 'state', bri=255, on=True, xy=[0.3426, 0.1705])
+            b.lights(4, 'state', bri=255, on=True, xy=[0.3426, 0.1705])
+            print("Field goal")
         elif score_diff == 1:
-            print("extra point")
-            if cheer_time:
-                print("good stuff")
-            else:
-                print("bad stuff")
+            b.lights(1, 'state', bri=255, on=True, xy=[0.1722, 0.0567])
+            b.lights(4, 'state', bri=255, on=True, xy=[0.1722, 0.0567])
+            print("Extra point")
         else:
-            print("touchdown or starting")
-            if cheer_time:
-                print("good stuff")
-            else:
-                print("bad stuff")
+            b.lights(1, 'state', bri=255, on=True, xy=[0.3069, 0.3152])
+            b.lights(4, 'state', bri=255, on=True, xy=[0.3069, 0.3152])
+            print("Touchdown")
+        pretty_score = "Us: " + str(sea_score) + " Them: " + str(suck_score)
+        print pretty_score
 
 c = C()
 year, week = nflgame.live.current_year_and_week()
@@ -65,9 +90,7 @@ while game_on:
     for p in games:
        if p.nice_score().encode('ascii','ignore').find('SEA') > -1:
           stupid_var = p.nice_score().encode('ascii','ignore').split(" ")
-          if stupidest_var == stupid_var:
-            print(p)
-          elif stupidest_var != stupid_var:
+          if stupidest_var != stupid_var:
             stupidest_var = stupid_var
             if p.is_home('SEA'):
                 suck_team = p.away.encode('ascii','ignore')
@@ -81,18 +104,19 @@ while game_on:
             sea_score_last = sea_score
             suck_score_last = suck_score
             game_on = p.playing()
-            winning_team = p.winner.encode('ascii','ignore')
+            if game_on == False:
+                winning_team = p.winner.encode('ascii','ignore')
             current_score = p.nice_score().encode('ascii','ignore')
             f = open('nfl.html','w')
             f.write("The current score is " + current_score) # python will convert \n to os.linesep
             f.close()
     time.sleep(5)
 else:
-    print("Game is over!")
     if winning_team == 'SEA':
         ending_text = "We won! Congrats everyone! The score was " + current_score
     else:
         ending_text = "We will get them next time! " + current_score
+    print(ending_text)
 f = open('nfl.html','w')
-f.write(ending_text + "\n") # python will convert \n to os.linesep
+f.write(ending_text + "\n")
 f.close()
