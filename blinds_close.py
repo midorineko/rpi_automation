de278f03bdc2ce91ac059463923dd510dbfbfd0d
midorineko <steven.inouye91@gmail.com>
2017-01-10 22:55:11 -0800

    fixing blinds

--- blinds_close.py
+++ blinds_close.py
@@ -8,11 +8,11 @@ last_line = f.readlines()[-1].rstrip()
 f.close()
 
 if last_line == 'open':
+  f = open("blinds.txt", "w")
+  f.write('close' + "\n")
+  f.close()
   SERVO = 17
   pi = pigpio.pi()
   pi.set_servo_pulsewidth(SERVO, 1000)
   time.sleep(1.53)
   pi.set_servo_pulsewidth(SERVO, 1500)
-  f = open("blinds.txt", "w")
-  f.write('close' + "\n")
-  f.close()
--- blinds_open.py
+++ blinds_open.py
@@ -8,11 +8,11 @@ last_line = f.readlines()[-1].rstrip()
 f.close()
 
 if last_line == 'close':
+  f = open("blinds.txt", "w")
+  f.write('open' + "\n")
+  f.close()
   SERVO = 17
   pi = pigpio.pi()
   pi.set_servo_pulsewidth(SERVO, 2000)
   time.sleep(1.53)
   pi.set_servo_pulsewidth(SERVO, 1500)
-  f = open("blinds.txt", "w")
-  f.write('open' + "\n")
-  f.close()
