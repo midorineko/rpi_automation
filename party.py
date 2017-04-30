71b1c9da28ed0c26d559fee895afa2053b259ca7
midorineko <steven.inouye91@gmail.com>
2017-04-30 19:03:33 +0000

    from the rpi

--- party.py
+++ party.py
@@ -1,7 +1,7 @@
 import sys
 import time
 from qhue import Bridge
-b = Bridge("192.168.0.102", "elbLovRPUcHaqss904iEJMH9LZrRwsvFeOKSfvOP")
+b = Bridge("192.168.0.101", "elbLovRPUcHaqss904iEJMH9LZrRwsvFeOKSfvOP")
 lights = b.lights
 
 command = sys.argv[-1]
